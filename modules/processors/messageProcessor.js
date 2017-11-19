0457a01170398e4754bb206d64cc4195c120c3d5
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2017-11-19 13:39:21 -0600

    Scenario bugfixing

--- modules/processors/messageProcessor.js
+++ modules/processors/messageProcessor.js
@@ -1,17 +1,17 @@
-const AutoRegularProcessor = require('./autoRegularProcessor');
+const AutoRoleProcessor = require('./autoRoleProcessor');
 const CustomCommandProcessor = require('./customCommandProcessor');
 const config = require('../../config.json');
 
 module.exports = class MessageProcessor {
 	constructor(client) {
 		this.discordJsClient = client;
-		this.autoRegularProcessor = new AutoRegularProcessor();
+		this.autoRoleProcessor = new AutoRoleProcessor();
 		this.customCommandProcessor = new CustomCommandProcessor(client);
 	}
 
 	process(message) {
-		if (config.autoRegular.enabled) {
-			this.autoRegularProcessor.process(message, this.discordJsClient);
+		if (config.autoRoles.enabled) {
+			this.autoRoleProcessor.process(message, this.discordJsClient);
 		}
 		this.customCommandProcessor.process(message);
 	}
